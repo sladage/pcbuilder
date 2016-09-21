826db384ced7b063af99f5d67fce07c144c993d5
Stefan Ladage <sladage@gmail.com>
2016-09-21 21:04:02 +0200

    Crawl that shit.

--- analyse/crawlertest/bin/crawler.dart
+++ analyse/crawlertest/bin/crawler.dart
@@ -1,16 +1,74 @@
 import 'package:crawlertest/crawler.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
 
-class Test implements PageWorker {
+List _moederborden = [];
+
+class Motherboard {
+  String name;
+  String formFactor;
+  String socket;
+  double price;
+  List<String> keywords = [];
+  Map toJson() => {
+        "name": name,
+        "formFactor": formFactor,
+        "socket": socket,
+        "price": price,
+        "keywords": keywords
+      };
+}
+
+//euro prijs naar double
+double prijsje(String prijs) {
+  List<int> temp = [];
+  for (int codeUnit in prijs.codeUnits) {
+    if (codeUnit >= 48 && codeUnit <= 57) {
+      temp.add(codeUnit);
+    } else if (codeUnit == 44) {
+      temp.add(46);
+    } else if (codeUnit == 45) {
+      temp.add(48);
+    }
+  }
+  return double.parse(new String.fromCharCodes(temp));
+}
+
+class Moederborden implements PageWorker {
   parse(Document document) {
-    var pp = document.querySelectorAll("p");
-    print("We have ${pp.length} P's");
+    var rows = document.querySelectorAll("div.listRow");
+    for (Element listRow in rows) {
+      Motherboard bord = new Motherboard();
+      bord.name = listRow.querySelector("span.name").text.trim();
+      var fields = listRow.querySelectorAll("span.info");
+      bord.formFactor = fields[0].text.trim();
+      bord.keywords.addAll(fields[1].text.split(","));
+      bord.socket = fields[2].text.trim();
+      bord.price = prijsje(listRow.querySelector("span.price").text);
+      bord.keywords
+          .addAll(listRow.querySelector("span.additional").text.split(","));
+      for (int i=0;i<bord.keywords.length;i++) {
+        bord.keywords[i] = bord.keywords[i].trim();
+      }
+      _moederborden.add(bord);
+    }
+
+    // meerdere pagina's?
   }
 }
 
-void main(List<String> args) {
-  Test worker = new Test();
-  try{
-    Crawler.crawl("https://tweakers.net/", worker);
+main(List<String> args) async {
+  Moederborden worker = new Moederborden();
+
+  try {
+    await Crawler.crawl(
+        "https://www.alternate.nl/Hardware-Componenten-Moederborden-Intel/html/listings/11626?lk=9435&size=500&showFilter=true#listingResult",
+        worker,referer: "https://www.alternate.nl/Moederborden/Intel",/*cookies:cookies*/);
+    String json = new JsonEncoder.withIndent("  ").convert(_moederborden);
+    print("We hebben ${_moederborden.length} moederborden gevonden.");
+    File uit = new File("moederborden.json");
+    uit.writeAsStringSync(json);
   } catch (e) {
     print(e);
   }
--- analyse/crawlertest/lib/crawler.dart
+++ analyse/crawlertest/lib/crawler.dart
@@ -11,14 +11,31 @@ abstract class PageWorker {
 }
 
 class Crawler {
-  static Future crawl(String url, PageWorker worker, {Map postData}) async {
+  static Future crawl(String url, PageWorker worker, {Map postData, String referer, Map<String,String> cookies}) async {
     String content;
-    if (postData == null) {
-      content = await Http.read(url);
-    } else {
-      content = (await Http.post(url,body: postData)).body;
+    Map headers = {};
+    headers["Host"] = Uri.parse(url).host;
+    headers["Cache-Control"] = "max-age=0";
+    headers["Upgrade-Insecure-Requests"] = "1";
+    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+    headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+    if (referer != null) headers["Referer"] = referer;
+    headers["Accept-Encoding"] = "gzip, deflate, sdch, br";
+    headers["Accept-Language"] = "nl-NL,nl;q=0.8,en-US;q=0.6,en;q=0.4,de-DE;q=0.2";
+    if (cookies != null) {
+      String cookiesstr = "";
+      for (String key in cookies.keys) {
+        cookiesstr += "$key=\"${cookies[key]}\";";
+      }
+      headers["Cookie"] = cookiesstr;
     }
+    headers["DNT"] = "1";
 
+    if (postData == null) {
+      content = await Http.read(url,headers: headers);
+    } else {
+      content = (await Http.post(url,body: postData,headers: headers)).body;
+    }
     Document document = HtmlParser.parse(content);
     worker.parse(document);
   }
